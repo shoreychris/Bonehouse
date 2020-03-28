185d75af3e08d6143128456f44dd7df701a19172
shoreychris <58793322+shoreychris@users.noreply.github.com>
2020-03-28 12:24:30 +0000

    Add files via upload

---
+++ about.php
@@ -0,0 +1,69 @@
+<?php
+include "topper.php";
+?>
+<link rel="stylesheet" href="HD4.css" type="text/css">
+<div class="about-section">
+  <h1>About Us Page</h1>
+  <p>Barker & Bonehouse is a friendly dog barbers which offers more.<br>
+		We offer salon and mobile grooming services,<br> online shop and pet photography.</p>
+</div>
+<body>
+<h2 style="text-align:center">Our Team</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="WSPics/nic.jpg" alt="Nicola" style="width:100%">
+      <div class="container">
+        <h2>Nicola Swift-Hunter</h2>
+        <p class="title">Manager</p>
+        <p>Nicola is our owner and manger with over 12 years of grooming
+		experience. Nicola is also our resident photographer.</p>
+        <p>N.SwiftHunter@gmail.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="WSPics/sharon.jpg" alt="Sharon" style="width:100%">
+      <div class="container">
+        <h2>Sharon Ross</h2>
+        <p class="title">Dog Groomer</p>
+        <p>Sharon has been with us for 10 years now and does both salon and mobile grooming.
+		</p>
+        <p>S.Ross@gmail.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="WSPics/kate.jpg" alt="Kate" style="width:100%">
+      <div class="container">
+        <h2>Kate Smith</h2>
+        <p class="title">Dog Groomer/Dog Daycare supervisor</p>
+        <p>Kate has been with us for 2 years and is fantastic with the dogs.
+			Kate works with the dogs in the salon and daycare.</p>
+        <p>K.Smith@gmail.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+<div class="column">
+    <div class="card">
+      <img src="WSPics/team.jpg" alt="Kate" style="width:100%">
+      <div class="container">
+        <h2>Our support team</h2>
+        <p class="title">Helpers</p>
+        <p>Meet our support team who are pivitol to our success.
+		This lovely band of people are organised with the help of Tim Loft (far left). 
+		Duties include help with running our online shop, doggy daycare, walks and washing of the dogs. </p>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+<?php
+include "base.php";
