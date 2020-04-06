5516f61c0d5af90088b8af89f7bb0d23dea033dc
shoreychris <58793322+shoreychris@users.noreply.github.com>
2020-04-06 14:59:10 +0100

    Update welfare.php

--- welfare.php
+++ welfare.php
@@ -4,11 +4,23 @@ include "topper.php";
 
 <div class=content>
 <link rel="stylesheet" href="HD.css" type="text/css">
+<br>
+<center> GROOMING TIPS <br> <img src="WSPics/groom2.jpg" width=600 height=600 align=auto></center>
+
+<br>
+
+<center> DIET <br> <img src="WSPics/diet.jpg" width=450 height=800 align=auto></center>
 
 <div class="container">
 
-
-
+<br>
+	<center>
+	<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/q-TgwGk1Btg" frameborder="2" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  	<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Msivkk7e4L0" frameborder="2" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  	<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/aJAh4M3k9sw" frameborder="2" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</center>
+<br>
+<center>If you have any questions please call our dedicated team on 01912636538.</center>
 </div>
 <div class="container">
 <br>
