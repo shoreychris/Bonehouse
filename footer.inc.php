0a6e5701cf0162fe9abc7be63b8ce4b56a573aca
shoreychris <58793322+shoreychris@users.noreply.github.com>
2020-04-06 13:45:36 +0100

    Update footer.inc.php

--- footer.inc.php
+++ footer.inc.php
@@ -1,17 +1,15 @@
-<div class="clearfix"></div>
-         <footer class="site-footer">
-            <div class="footer-inner bg-white">
-               <div class="row">
-                  <div class="col-sm-6">
+
+            <div class="row">
+                <div class="col-sm-6">
                   <div class="col-sm-6 text-right">
-                     <a href="cal-index.php" class="btn btn-success pull-left">Calendar</a>
-                           <br><br>
+				     <a href="cal-index.php" class="btn btn-success pull-left">Calendar</a>
+				       <br><br>
                      <a href="admin.php" class="btn btn-success pull-left">Admin home page</a>
+					 
                   </div>
                </div>
             </div>
          </footer>
-      </div>
       <script src="assets/js/vendor/jquery-2.1.4.min.js" type="text/javascript"></script>
       <script src="assets/js/popper.min.js" type="text/javascript"></script>
       <script src="assets/js/plugins.js" type="text/javascript"></script>
