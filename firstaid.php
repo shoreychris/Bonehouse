6c94cf8537127aa062f12387c7c3a757445b4896
shoreychris <58793322+shoreychris@users.noreply.github.com>
2020-04-06 14:59:59 +0100

    Update firstaid.php

--- firstaid.php
+++ firstaid.php
@@ -15,9 +15,9 @@ include "topper.php";
 
 <br>
 	<center>
-	<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/KqClwoUrgZA"></iframe>
-  	<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/0o8BEkIvgqA"></iframe>
-  	<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/gT_vNktCbyw"></iframe>
+	<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/KqClwoUrgZA"frameborder="2" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  	<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/0o8BEkIvgqA"frameborder="2" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  	<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/gT_vNktCbyw"frameborder="2" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	</center>
 <br>
 <center>If you have any questions please call our dedicated team on 01912636538.</center>
